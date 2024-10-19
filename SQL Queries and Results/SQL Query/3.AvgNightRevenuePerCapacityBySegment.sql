b05d430549e3b9cdf81b5ed3dfe6006afefa7c89
Alex <110292180+Alexsometimescode@users.noreply.github.com>
2024-10-20 00:23:29 +0200

    Update 3.AvgNightRevenuePerCapacityBySegment.sql

--- SQL Queries and Results/SQL Query/3.AvgNightRevenuePerCapacityBySegment.sql
+++ SQL Queries and Results/SQL Query/3.AvgNightRevenuePerCapacityBySegment.sql
@@ -3,12 +3,12 @@ DROP VIEW IF EXISTS AvgNightRevenuePerCapacityBySegment;
 CREATE VIEW AvgNightRevenuePerCapacityBySegment AS
 SELECT 
 	res.NationalityCode,
-    round(AVG(CASE WHEN res.OccupiedSpace_Sum > 0 
+    	round(AVG(CASE WHEN res.OccupiedSpace_Sum > 0 
              THEN res.NightCost_Sum / res.OccupiedSpace_Sum 
              ELSE 0 END),1) AS AvgNightRevenuePerCapacity
 FROM 
-    reservations res
+    	reservations res
 GROUP BY 
-		res.NationalityCode
+	res.NationalityCode
 ORDER BY 
-    AvgNightRevenuePerCapacity DESC;
+    	AvgNightRevenuePerCapacity DESC;
